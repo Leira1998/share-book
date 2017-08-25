0c40a9d65b3b397428d329892bb955598723fa10
Fernando Leira <LeiraFernandoCortel@gmail.com>
2017-08-25 11:35:37 -0400

    Books controller update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:success] = "Book Created!"
       redirect_to @book
     else
       render "new"
@@ -27,17 +28,29 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      flash[:success] = "Book Updated!"
+      redirect_to(:action => 'show', :id => @book.id)
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to root_path
   end
 
   private
   def book_params
-    params.require(:book).permit(:id)
+    params.require(:book).permit(:id, :title, :body, :image)
   end
 
 end
