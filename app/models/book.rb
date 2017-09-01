47b2586c37bb3aa807024736325171ef09d70cac
Fernando Leira <LeiraFernandoCortel@gmail.com>
2017-08-31 21:27:26 -0400

    Thumb problem fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_attached_file :document, url: "/pdf/:hash.:extension", hash_secret: "abc123"
+  has_attached_file :document, url: "/pdf/:hash.:extension", hash_secret: "abc123", style: { :thumb => ["200x200#", :jpg], :medium => ["500x500>", :jpg] }
   validates_attachment :document, :content_type => { :content_type => ["application/pdf"] }
 
   #has_attached_file :image, url: "/img/:hash.:extension", hash_secret: "abc123", style: { thumb: ["64x64#", :png], original: ["500x500>", :jpg] }
