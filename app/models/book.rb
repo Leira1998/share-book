21b31b08557044180fd964f1302a0818df8b56db
Fernando Leira <LeiraFernandoCortel@gmail.com>
2017-09-19 08:26:30 -0400

    Dropbox Plug-In

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
-  has_attached_file :document, url: "/pdf/:hash.:extension", hash_secret: "abc123", style: { :thumb => ["200x200#", :jpg], :medium => ["500x500>", :jpg] }
+  has_attached_file :document,
+    storage: :dropbox,
+    dropbox_credentials: Rails.root.join("config/dropbox.yml"),
+    dropbox_options: {},
+    url: "/pdf/:hash.:extension",
+    hash_secret: "abc123",
+    style: { :thumb => ["200x200#", :jpg], :medium => ["500x500>", :jpg] }
   validates_attachment :document, :content_type => { :content_type => ["application/pdf"] }
 
   #has_attached_file :image, url: "/img/:hash.:extension", hash_secret: "abc123", style: { thumb: ["64x64#", :png], original: ["500x500>", :jpg] }
